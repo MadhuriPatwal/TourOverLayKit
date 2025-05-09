f63ab7371ce46d7cdb1e09b3fbc78fb7cd595eb7
MadhuriPatwal <madhuri.patwal@livestockinformation.org.uk>
2025-05-09 16:57:40 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "TourOverlayKit",
     platforms: [
-            .iOS(.v14) // or v13 if needed
+            .iOS(.v13) // or v13 if needed
         ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
